e930d2b67d0a18f416978797c1459320afecdbd3
Tom4FirmwareEng <w364th@gmail.com>
2020-05-11 12:07:19 -0700

    1-wire device driver
    1-wire device driver

--- 1-wire_devicedriver.c
+++ 1-wire_devicedriver.c
@@ -82,4 +82,46 @@ int SMB_RdByte_1W(unsigned char *dest_addr, unsigned char SlaveAddr)
     return 1;
 }
 
+///////////////////////////// Exported routines API ///////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+//-----------------------------------------------------------------------------
+unsigned char DSS_Init(void)
+{
+    unsigned char RdData = 0xFF;
+    unsigned char RdData64 = 0xFF, RdData65 = 0xFF;
+    
+    // per data sheet set the master to over-drive mode to match the slave
+    SMB_WrByte_1W(DSS_REG_CFG, (DSS_OVERDRIVE + DSS_STRONGPULLUP), ONEWIREMASTER_ADDR); // write to config register
+    //SMB_WrByte_1W(DSS_REG_CFG, (DSS_OVERDRIVE), ONEWIREMASTER_ADDR); // write to config register
+    Wait_ms(3);
+    SMB_WrByte_1W(DSS_REG_CMD, CMD_1WRS, ONEWIREMASTER_ADDR); // write 1W-RST commmand to cmd reg.
+    Wait_ms(3);
+    SMB_RdByte_1W(&RdData, ONEWIREMASTER_ADDR);    // just read to a dumy byte, lsb should be cleared
+    Wait_ms(1);
+    
+    // read Protection Status Register 64h in the Master device
+    SMB_WrByte(DSS_REG_PSTATUS64, ONEWIREMASTER_ADDR, 0); // read RP1, WP1, RP0, WP0
+    Wait_ms(1);
+    SMB_RdByte_1W(&RdData64, ONEWIREMASTER_ADDR); // read data byte
+    RdData64 &= 0xF0;
+    Wait_ms(1); 
+    SMB_WrByte(DSS_REG_PSTATUS65, ONEWIREMASTER_ADDR, 0); // read RP3, WP3, RP2, WP2
+    RdData65 &= 0xF0;
+    Wait_ms(1);
+    SMB_RdByte_1W(&RdData65, ONEWIREMASTER_ADDR); // read data byte
+    Wait_ms(1);    
+    
+    return RdData;    // should be 0x0A: device presence is true, bus is not busy    
+}
+
+//-----------------------------------------------------------------------------
+int DSS_Reset(unsigned char* RdData)
+{   
+    SMB_WrByte_1W(DSS_REG_CMD, CMD_1WRS, ONEWIREMASTER_ADDR); // write 1W-RST commmand to cmd reg.
+    delay_us4(50);
+    SMB_RdByte_1W(RdData, ONEWIREMASTER_ADDR);    // just read to a dummy byte, lsb should be cleared
+    delay_us4(20);
+    return 0;   // no error 
+}
 
